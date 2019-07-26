66fa10789d3d31244e216f508100ad9a35d90836
Yan Mingzhi <yanmingzhi@elastos.org>
2019-07-26 13:56:52 +0800

    add node deployment

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :main jepsen.elastos-cli
+  :jvm-opts ["-Dcom.sun.management.jmxremote"]
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [jepsen "0.1.13"]
+                 [jepsen "0.1.13-SNAPSHOT"]
                  [verschlimmbesserung "0.1.3"]]
   :repl-options {:init-ns jepsen.elastos-cli})
--- src/jepsen/elastos_cli.clj
+++ src/jepsen/elastos_cli.clj
@@ -12,8 +12,51 @@
             [jepsen.os.debian :as debian]
             [slingshot.slingshot :refer [try+]]))
 
+(def dir "/opt/elastos")
+(def binary "ela")
+(def cli-binary "ela-cli")
+(def logfile (str dir "/ela.log"))
+(def pidfile (str dir "/ela.pid"))
+
+(defn ela-node
+  "Ela node for a particular version."
+  [version]
+  (reify
+   db/DB
+   (setup! [_ test node]
+           (c/su
+            (info node "installing ela" version)
+            (let [url (str "http://172.16.0.120/ela.zip")]
+              (cu/install-archive! url dir))
+
+            (cu/start-daemon!
+             {:logfile logfile
+              :pidfile pidfile
+              :chdir   dir}
+             binary)
+            )
+           )
+
+   (teardown! [_ test node]
+              (info node "tearing down ela")
+              (cu/stop-daemon! binary pidfile)
+              (c/su
+               (c/exec :rm :-rf dir))
+              )))
+
+
+(defn ela-test
+  "Given an options map from the command-line runner (e.g. :nodes, :ssh,
+  :concurrency, ...), constructs a test map."
+  [opts]
+  (merge tests/noop-test
+         opts
+         {:name "ela"
+          :os   debian/os
+          :db   (ela-node "v0.3.5")}))
+
 (defn -main
-  "Handles command line arguments. Can either run a test, or a web server for
-  browsing results."
+  ""
   [& args]
-  (prn "Hello, world!" args))
+  (cli/run! (cli/single-test-cmd {:test-fn ela-test})
+            args))
